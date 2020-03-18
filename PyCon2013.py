c1cfed10bd5cb1495ff7dbbdc3c806c3c7107256
Alanna Genin <alannagenin@gmail.com>
2020-03-18 12:21:40 +0100

    Update PyCon 2013

--- PyCon2013.py
+++ PyCon2013.py
@@ -12,3 +12,23 @@ Created on Tue Mar 17 10:37:34 2020
 print("Square : ")
 for i in [0, 1, 2, 3, 4, 5, 6]:
    print(i**2)
+
+#better 
+print("\nBetter version :")   
+for i in range(7):
+    print(i**2)
+
+#better but ugly --> doesn't exists anymore
+#for i in xrange(7):
+#    print(i**2)
+    
+#Looping over a collection    
+print("\nColors :")
+colors = ['red', 'green', 'blue', 'yellow']
+for i in range(len(colors)):
+    print(colors[i])
+    
+#better and faster
+print("\nBetter version for colors :")
+for color in colors:
+    print(color) 
