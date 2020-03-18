9cbf250da5ac5960114c481c24cb65e69a7ae6c6
Alanna Genin <alannagenin@gmail.com>
2020-03-18 11:51:49 +0100

    Upload PyCon 2013

---
+++ PyCon2013.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 10:37:34 2020
+
+@author: alanna
+"""
+
+#tools to use : iter, partial
+
+#Looping integers
+print("Square : ")
+for i in [0, 1, 2, 3, 4, 5, 6]:
+   print(i**2)
