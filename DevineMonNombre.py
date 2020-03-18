fc91634bcf45e93d59bf719ceab180b1289a7f94
Alanna Genin <alannagenin@gmail.com>
2020-03-18 16:10:57 +0100

    Jeu pour deviner un nombre

---
+++ DevineMonNombre.py
@@ -0,0 +1,44 @@
+from random import randint
+
+nbr_essais_max = 5
+nbr_essais = 1
+
+#-------------------------------- Choix min max --------------------------------
+
+print("\n-------------- Choix des bornes -------------- ", end='')
+
+borne_inf, borne_sup = int(input("Min : ")), int(input("Max : "))
+
+mon_nombre = randint(borne_inf, borne_sup)
+ton_nombre = 0
+
+#-------------------------------- Debut du jeu --------------------------------
+
+print("\nJ'ai choisi un nombre entre ", borne_inf, " et ", borne_sup, ".")
+print("A vous de le deviner en",nbr_essais_max,"tentatives au maximum !")
+
+
+print("\n-------------- Vos propositions -------------- ")
+while ton_nombre != mon_nombre and nbr_essais <= nbr_essais_max:
+    print("\nEssai n°",nbr_essais, end='')
+    while True:
+        try:
+            ton_nombre = int(input("Votre proposition : "))
+            break
+        except ValueError:
+            print("Réponse non valide. Réessayez !")
+    if ton_nombre < mon_nombre:
+        print("Trop petit")
+    elif ton_nombre > mon_nombre:
+        print("Trop grand")
+    else:
+        if nbr_essais == 1 :
+            print("Bravo ! Vous avez trouvé", mon_nombre, "en", nbr_essais,"essai.")
+        else:
+            print("Bravo ! Vous avez trouvé", mon_nombre, "en", nbr_essais,"essais.")
+    nbr_essais += 1
+            
+        
+if nbr_essais>nbr_essais_max and ton_nombre != mon_nombre :
+    print("\nDésolé, vous avez utilisé vos",nbr_essais_max,"essais en vain.")
+    print("J'avais choisi le nombre ", mon_nombre,".", end='', sep='')
