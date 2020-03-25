349689c41355f556277c7aa7da6aca2a7783690d
Alanna Genin <alannagenin@gmail.com>
2020-03-25 11:11:05 +0100

    Ouput function and update play function

--- Hangman.py
+++ Hangman.py
@@ -7,80 +7,54 @@ Created on Fri Mar 20 19:31:50 2020
 """
 
 import csv
-from random import randint
+from random import choice #randint
+from collections import Counter
 
-if __name__ == "__main__":
-    print("This game is a hangman. Try to guess my word.")
-
+#open the file with the words
 with open("english-common-words.csv") as csvfile:
     words = csv.reader(csvfile, delimiter=",")
-    words = [",".join(word) for word in words]  # put words in a list
-    words = [word for word in words if len(word) > 2]  # select only words > 2 letters
-
+    words = [",".join(word) for word in words] # put words in a list
+    words = [word for word in words if len(word) > 2] # select only words > 2 letters
 
 # choose randomly a word
 def choose_word(words):
-    word = words[randint(0, len(words))]
+    # word = words[randint(0, len(words))]
+    word = choice(words)
     return word.upper()
-    # This aleady exits, it's random.choice().
 
+word = choose_word(words)
+#print(word)
 
-# return as many _ as letters in the word
+# print as many _ as letters in the word
 def start(word):
     """
     >>> start('hello_world')
     None
     """
-    # This does not returns
-    for _ in word:
-        # This iterates on every letter of the word, put the letter in the _ variable
-        # if _ == '_': xxxxx
-        # the _ variable is tricky be careful.
-        # word.count('_')  is a better option.
-        print("_", sep=" ", end=" ")
-
-
-# No this does a print, it returns no
-
-word = choose_word(words)
-print(word)
+    underscore = ''
+    for i in range(len(word)):
+        underscore += '_ '
+    return(underscore)
 
 # tries
 state = {
-    "max": len(word) + 5,
-    "remaining": len(word) + 5,
+    "max": len(word) + 3,
+    "remaining": len(word) + 3,
     "guesses": "",
     "failed": 0,
     "solution": word,
 }
 
-
-def play(character, word):
-    state["remaining"] -= 1
-
-    if state["remaining"] == 0:
-        print(
-            "\nYou lose, you used your ",
-            state["max"],
-            " tries. \nMy number was ",
-            state["solution"],
-            ".",
-            sep="",
-        )
-        return False
-
-    elif character not in word:
-        print("Wrong. You have", state["remaining"], "more guesses.")
-        print("_", sep=" ", end=" ")
-        state["failed"] += 1
-        return True
-
-    elif character in word:
-        print(character, sep=" ", end=" ")
-        return True
+#def error(character):
+#    if not character.isalpha():
+#        print("Enter only a letter")
+#    elif len(character) > 1:
+#        print("Enter only a single letter")
+#    elif character in state["guesses"]:
+#        print('You have already guessed that letter') 
+#    return False
 
 
-# hint: something
 def output(guessed, solution):
     """
     >>> output('des', 'descartes')
@@ -90,31 +64,52 @@ def output(guessed, solution):
     """
     for letter in solution:
         if letter in guessed:
-            print(letter, end="")
+            print(letter, end="", sep=" ")
         else:
-            print("_", end="")
+            print(" _ ", end="", sep=" ")
+    print()
 
 
-# you can also do
+def play(character, word):
+    #error(character)
+    state["remaining"] -= 1
+    state["guesses"] += character
+    
+    #print the propositions
+    output(state["guesses"], state["solution"])
+    
+    #if all the letters are in the word return False
+    if Counter(state["guesses"]) == Counter(word):
+        print("\nYou win! \nMy word was ", state["solution"],".",sep="")
+        return False
+    
+    #stop if no more tries remaining
+    if state["remaining"] <= 0:
+        print("\nYou lose, you used your ", state["max"]," tries. \nMy word was ",
+            state["solution"],".",sep="")
+        return False
+    
+    #if the character is not found return True
+    if character not in word:
+        print("Wrong. You have", state["remaining"], "more guesses.")
+        state["failed"] += 1
+        return True
+
+    #if the character is found return True
+    elif character in word:
+        if word.count(character) > 1:
+            state["guesses"] = word.count(character)*character
+    return True
 
 
-def output2(guessed, solution):
-    """
-    >>> output2('n', 'Bonjour')
-    '__n____'
-    >>> output2('b', 'Bonjour')
-    'b______'
-
-    #this will fail, fix the code so that it works
-
-    """
-
-    return "".join(letter if letter in guessed else "_" for letter in solution)
+    
 
 
-#
-# -------------------------------- Loop --------------------------------
+
+# ----------------------------------- Loop ------------------------------------
 if __name__ == "__main__":
+    print("This game is a hangman, try to guess my word.")
+
     print("\n-------------- Your propositions -------------- ")
     print("\nYou have", state["remaining"], "tries to guess : ", start(word), sep=" ")
 
@@ -122,9 +117,7 @@ if __name__ == "__main__":
         char = str(input("Proposition : "))
         res = play(char.upper(), state["solution"])
         if res:
-            print(
-                "\nYou have ", state["remaining"], " tries remaining.", end="", sep=""
-            )
+            print("\nYou have ", state["remaining"], " tries remaining.", end="", sep="")
         else:
-            # stop the game if false (no more tries or number found)
+            # stop the game if false (no more tries or word found)
             break
