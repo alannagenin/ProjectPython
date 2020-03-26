b9b912923263a210ad114fb6ad46b0e5196a1d62
Alanna Genin <alannagenin@gmail.com>
2020-03-26 14:55:10 +0100

    New functions + change code structure
    Boundaries, create_status, number_input and print_process

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -8,28 +8,43 @@ Created on Fri Mar 20 17:25:34 2020
 
 from random import randint
 
-if __name__ == '__main__' :
-    print("The aim of the game is too find a integer between two numbers that you choose. I will give you clues to find my number.")
-print("\n----------------- Min and max ----------------- ")
+#-------------------------------- Min and max ---------------------------------   
 
+def boundaries():
+    print("\n----------------- Min and max ----------------- ")
+    lower_boundary, upper_boundary = int(input("Min : ")), int(input("Max : "))
+    return lower_boundary, upper_boundary
 
-#-------------------------------- Min and max --------------------------------   
-
-lower_boundary, upper_boundary = int(input("Min : ")), int(input("Max : "))
-
-state = {"max": 5,
+def create_status(lower_boundary, upper_boundary):
+    state = {"max": 5,
          "remaining": 5 ,
          "solution": randint(lower_boundary, upper_boundary)}
+    
+    #give more tries
+    if upper_boundary - lower_boundary >= 1000:
+        state["max"] = state["remaining"] = randint(12, 15)
+    elif upper_boundary - lower_boundary >= 500:
+        state["max"] = state["remaining"] = randint(7, 12)
+    elif upper_boundary - lower_boundary >= 100:
+        state["max"] = state["remaining"] = randint(6,7)
+    
+    return state
 
-#give more tries
-if upper_boundary - lower_boundary >= 1000:
-    state["max"] = state["remaining"] = randint(12, 15)
-elif upper_boundary - lower_boundary >= 500:
-    state["max"] = state["remaining"] = 10
-elif upper_boundary - lower_boundary >= 100:
-    state["max"] = state["remaining"] = randint(6,7)
+#----------------------------------- Number -----------------------------------
 
-#-------------------------------- Process --------------------------------   
+def number_input():
+    number = int(input("Proposition : "))
+    
+    #testing is the number in between the two boundaries
+    while number < lower_boundary or number > upper_boundary:
+        number = int(input("Error, choose another number : "))
+        print(end='')
+        # if print() then it jumps two lines but I want only one line of separation
+
+    return number
+        
+
+#---------------------------------- Process -----------------------------------   
 
 def process(number, state):
     #decrease the number of tries
@@ -37,8 +52,25 @@ def process(number, state):
     
     #stop the game if there's no more moves left
     if state["remaining"] == 0 and number != state["solution"]:
-        print("\nYou lose, you used your ", state["max"], " tries. \nMy number was ", state["solution"], ".", sep='')
         return False
+    
+    #stop if number found
+    if number == state["solution"]:
+        return False
+     
+    #too big
+    if number > state["solution"]:
+        return True
+    
+    #too small
+    if number < state["solution"]:
+        return True
+
+def print_process(number, state):
+    #stop the game if there's no more moves left
+    if state["remaining"] == 0 and number != state["solution"]:
+        print("\nYou lose, you used your ", state["max"], " tries. \nMy number was ", state["solution"], ".", sep='')
+
     
     #stop if number found
     if number == state["solution"]:
@@ -46,33 +78,41 @@ def process(number, state):
             print("You win in ", state["max"] - state["remaining"], " try.", sep='')
         else:
             print("You win in ", state["max"] - state["remaining"], " tries.", sep='')
-        return False
-     
-    #too big
-    if number > state["solution"]:
-        print("Too big")
-        return True
+
+    if state["remaining"] != 0:
+        #too big
+        if number > state["solution"]:
+            print("Too big")
     
-    #too small
-    if number < state["solution"]:
-        print("Too small")
-        return True
+        
+        #too small
+        if number < state["solution"]:
+            print("Too small")
+
     
 
-#-------------------------------- Loop --------------------------------    
+#------------------------------------ Loop ------------------------------------    
 
-print("\n-------------- Your propositions -------------- ")
-print("\nYou have", state["remaining"], "tries to guess my number.", sep=" ")
-while True:
-    number = int(input("Proposition : "))
-    while number < lower_boundary or number > upper_boundary:
-        number = int(input("Error, choose another number : "))
-        print(end='')
-    res = process(number, state)
-           
-    if res:
-        print("\nYou have ", state["remaining"], " tries remaining.", end='', sep='')
-    else :
-        #stop the game if false (no more tries or number found)
-        break
+if __name__ == '__main__' :
+    print("The aim of the game is too find a integer between two numbers that you choose.",
+          "I will give you clues to find my number.")
+    
+    #Setting up the game
+    lower_boundary, upper_boundary = boundaries()
+    state = create_status(lower_boundary, upper_boundary)
+    print("\nYou have", state["remaining"], "tries to guess my number.", sep=" ")
+    
+    print("\n-------------- Your propositions -------------- ")
+    while True: 
+        #propositions
+        number = number_input()
+        res = process(number, state)
+        print_process(number, state)
+        
+        #contiunue or stop guessing
+        if res:
+            print("\nYou have ", state["remaining"], " tries remaining.", end='', sep='')
+        else :
+            #stop the game if false (no more tries or number found)
+            break
 
